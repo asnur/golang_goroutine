f57c7b2adf84cc51c712b4085309ad953effe14d
Asnur Ramdani <asnurramdhani12@gmail.com>
2022-06-12 18:03:50 +0700

    Testing Deadlock

--- mutex_test.go
+++ mutex_test.go
@@ -56,3 +56,52 @@ func TestRWMutex(t *testing.T) {
 	time.Sleep(time.Second * 5)
 	fmt.Println("Balance = ", account.GetBalance())
 }
+
+//Deadlock Condition
+
+type UserBalance struct {
+	sync.Mutex
+	Name    string
+	Balance int
+}
+
+func (user *UserBalance) Lock(amount int) {
+	user.Mutex.Lock()
+}
+
+func (user *UserBalance) Unlock() {
+	user.Mutex.Unlock()
+}
+
+func (user *UserBalance) Change(amount int) {
+	user.Balance = user.Balance + amount
+}
+
+func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
+	user1.Lock(amount)
+	fmt.Println("User 1 locked ", user1.Name)
+	user1.Change(-amount)
+
+	time.Sleep(1 * time.Second)
+
+	user2.Lock(amount)
+	fmt.Println("User 2 locked ", user2.Name)
+	user2.Change(amount)
+
+	time.Sleep(1 * time.Second)
+
+	user1.Unlock()
+	user2.Unlock()
+}
+
+func TestDeadLock(t *testing.T) {
+	user1 := UserBalance{Name: "Asnur", Balance: 100000}
+	user2 := UserBalance{Name: "Amelia", Balance: 100000}
+
+	go Transfer(&user1, &user2, 100)
+	go Transfer(&user2, &user1, 200)
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("User 1", user1.Name, " balance = ", user1.Balance)
+	fmt.Println("User 2", user2.Name, " balance = ", user2.Balance)
+}
